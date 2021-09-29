2b3d2dc02cc77ed99d55b31e3d65a58b7f02a72e
Nathalie Baladejo-Reynosa <nathy.reynosa@protonmail.com>
2021-09-29 05:34:43 +0000

    changed class to className

--- pages/family-members/[id].js
+++ pages/family-members/[id].js
@@ -25,16 +25,16 @@ export async function getStaticProps({params}) {
 export default function Entry({itemData}) {
   return (
     <Layout entry>
-      <div class="card text-center">
-        <div class="card-body text-center">
-          <h5 class="card-title text-center">Hi my name is: {itemData.data.name}</h5>
-          <p class="card-text">I'm {itemData.data.age} years old</p>
-          <p class="card-text">My favorite hobby is: {itemData.data.hobby}</p>
-          <p class="card-text">{itemData.data.birthdate}</p>
-          <p class="card-text">{itemData.data.favorite_animal}</p>
-          <p class="card-text">{itemData.data.favorite_food}</p>
-          <p class="card-text">{itemData.data.favorite_verse_1}</p>
-          <a href="#" class="btn btn-primary">Nice to meet you!</a>
+      <div className="card text-center">
+        <div className="card-body text-center">
+          <h5 className="card-title text-center">Hi my name is: {itemData.data.name}</h5>
+          <p className="card-text">I'm {itemData.data.age} years old</p>
+          <p className="card-text">My favorite hobby is: {itemData.data.hobby}</p>
+          <p className="card-text">{itemData.data.birthdate}</p>
+          <p className="card-text">{itemData.data.favorite_animal}</p>
+          <p className="card-text">{itemData.data.favorite_food}</p>
+          <p className="card-text">{itemData.data.favorite_verse_1}</p>
+          <a href="#" className="btn btn-primary">Nice to meet you!</a>
         </div>
       </div>
     </Layout>
